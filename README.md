166802ee7fe299d8c5cb0bc49d63f2791fc97f8e
EnricoBDev <enricocubing@gmail.com>
2023-05-22 12:22:43 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# QuizApp
+
+Minimal quiz application to demonstrate what we studied during the 2022/2023 school year.
---
+++ src/main/java/com/mycompany/quizapp/QuizApp.java
@@ -0,0 +1,12 @@
+package com.mycompany.quizapp;
+
+/**
+ *
+ * @author EnricoBDev
+ */
+public class QuizApp {
+
+    public static void main(String[] args) {
+        
+    }
+}
