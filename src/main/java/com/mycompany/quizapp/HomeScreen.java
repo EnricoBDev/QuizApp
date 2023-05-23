5af1bb19fd0696412249aa5e00428d8eca5fe57d
Felics0 <05felice.pecchia@gmail.com>
2023-05-23 22:18:14 +0200

    prova

--- src/main/java/com/mycompany/quizapp/HomeScreen.java
+++ src/main/java/com/mycompany/quizapp/HomeScreen.java
@@ -6,7 +6,6 @@ import java.awt.FlowLayout;
 import java.awt.Graphics;
 import java.awt.GridLayout;
 import java.awt.Image;
-import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
